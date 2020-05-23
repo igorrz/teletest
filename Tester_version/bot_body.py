e19f97d72546717031507ac8b4fcd1ad8d33e9d6
IGOR <igor@IGORs-Air.fritz.box>
2020-05-23 23:01:55 +0200

    minor

--- Tester_version/bot_body.py
+++ Tester_version/bot_body.py
@@ -20,8 +20,8 @@ def start(update, context):
     usr_name=update.message.from_user.username
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"Privet, {usr_name}, pomahaemsa?")
 
-def echo(update, context):
-    update.message.reply_text('Di nah')
+#def echo(update, context):
+#    update.message.reply_text('Di nah')
 
 def get_temp(update,context):
     data_complete=[]
@@ -36,9 +36,9 @@ def get_temp(update,context):
 
 
 start_handler = CommandHandler('start', start)
-dp.add_handler(start_handler)
-dp.add_handler(MessageHandler(Filters.text, echo))
 graph_handler=CommandHandler('get_temp',get_temp)
+dp.add_handler(start_handler)
+#dp.add_handler(MessageHandler(Filters.text, echo))
 dp.add_handler(graph_handler)
 
 updater.start_polling()
