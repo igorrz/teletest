ebcdfab7bf16726ddc150b6b8ca6a2526c7a6f88
IGOR <igor@IGORs-Air.fritz.box>
2020-05-23 22:55:03 +0200

    minor

--- Tester_version/bot_body.py
+++ Tester_version/bot_body.py
@@ -2,9 +2,16 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler,MessageHandler,Filters
 from telegram.message import User
 import logging
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+import csv
+
 
 updater = Updater(token='823328456:AAEZqOTZcLnJywiza9PkvgtyHjdFjeFzenE', use_context=True)
 dp = updater.dispatcher
+data_time_directory='/home/pi/teletest/temp_measure/'
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -15,6 +22,17 @@ def start(update, context):
 
 def echo(update, context):
     update.message.reply_text('Di nah')
+
+def get_temp(update,context):
+    data_complete=[]
+    with open(data_time_directory+'date_time_temp.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            data_complete.append(row[2])
+        csvfile.close()
+    plt.plot([x for x in range(len(data_complete))],data_complete,color='r')
+    plt.savefig(data_time_directory+'plot.png')
+    update.bot.send_photo(chat_id=update.effective_chat.id, photo=open(data_time_directory+'plot.png', 'rb'))
 
 
 start_handler = CommandHandler('start', start)
