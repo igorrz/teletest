b655f1e615d92d232d1b9711768c1ad196aae58a
IGOR <igor@IGORs-Air.fritz.box>
2020-05-27 22:20:38 +0200

    Function to get last 6 and current temp

--- Tester_version/bot_body.py
+++ Tester_version/bot_body.py
@@ -25,24 +25,31 @@ def start(update, context):
 
 def get_temp(update,context):
     data_time,data_temp=read_date_temp_file(data_time_directory)
-    temp_plot(data_time,data_temp)
+    plt_title='The complete temperature plot'
+    temp_plot(data_time,data_temp,plt_title)
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(data_time_directory+'current_plot.png', 'rb'))
 
 
-def get_temp_last_24(update,context):
+def get_temp_last_6(update,context):
     data_time,data_temp=read_date_temp_file(data_time_directory)
-    data_time=data_time[-720:]; data_temp=data_temp[-720:]
-    temp_plot(data_time,data_temp)
+    data_time=data_time[-180:]; data_temp=data_temp[-180:]
+    plt_title='The temperature plot over last 6 hours'
+    temp_plot(data_time,data_temp,plt_title)
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(data_time_directory+'current_plot.png', 'rb'))
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Current Temperature: {data_temp[-1]}\n"+\
-        f'Last measure: {data_time[-1].time()}')
+    
 
+def get_current_temp(update,context):
+    data_time,data_temp=read_date_temp_file(data_time_directory)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Current Temperature: {data_temp[-1]}\n"+\
+        f'Last measure: {data_time[-1]}')
 
 start_handler = CommandHandler('start', start)
 graph_handler=CommandHandler('get_temp',get_temp)
-last_10=CommandHandler('overview',get_temp_last_24)
+last_6=CommandHandler('overview',get_temp_last_6)
+curr_temp=CommandHandler('current',get_current_temp)
 dp.add_handler(start_handler)
-dp.add_handler(last_10)
+dp.add_handler(last_6)
+dp.add_handler(curr_temp)
 #dp.add_handler(MessageHandler(Filters.text, echo))
 dp.add_handler(graph_handler)
 
@@ -56,10 +63,10 @@ Here bot unrelated backend functions
 
 '''
 
-def temp_plot(time,temp):
+def temp_plot(time,temp,title):
     datetime_obj=[matplotlib.dates.date2num(datetime.strptime(i,'%H:%M:%S')) for i in time]
     plt.plot(datetime_obj,temp,color='r')
-    plt.title('The temperature plot')
+    plt.title(title)
     plt.xlabel('Time'); plt.ylabel('Temperature in Grad Celsius')
     plt.gca().xaxis.set_major_formatter(pltdates.DateFormatter('%H:%M'))
     plt.savefig(data_time_directory+'current_plot.png')
